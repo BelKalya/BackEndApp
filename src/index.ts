018b87ade5888ed546c36e473fb1ecdd276d3fb3
Belk <andriiperervin@gmail.com>
2022-01-24 13:38:20 +0300

    Test Commit

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import {createConnection} from "typeorm";
 import {User} from "./entity/User";
 
 const main = async () => {
+    console.log('test');
     const conn = await createConnection({
         type: "postgres",
         url: process.env.DATABASE_URL,
